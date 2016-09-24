cfd0de7c957271e93e0ebc2e797fbc92420493e9
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-09-24 10:24:41 +0200

    finished events lists

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -5,6 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './market.component.html',
 })
 export class MarketComponent implements OnInit {
+  collectables = [
+    {description: 'A very rare copy of "jQuery for Dummies"', type: 'Book'},
+    {description: 'The first Letter ever written', type: 'Piece of Paper'},
+    {description: 'A photograph showing nothing', type: 'Photo'},
+    {description: 'A box with all sold Zunes', type: 'Garbage'}
+  ];
+
+  onAddToCollection() {
+    alert('Hello!');
+  }
 
   constructor() { }
 
